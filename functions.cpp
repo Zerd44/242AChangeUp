4a6baec6a32cf630e677597027eee4f2c70cf386
Zublo54 <56134365+Zublo54@users.noreply.github.com>
2021-05-12 15:59:22 -0400

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,31 +1,6 @@
 #include "main.h"
 
-// void master(){
-//   if(indexIn.isPressed()){
-//     indexBottom.moveVelocity(600);
-//     indexTopTwo.moveVelocity(600);
-//   }
-//   else if (allOut.isPressed()){
-//     indexBottom.moveVelocity(-600);
-//     indexTopTwo.moveVelocity(-600);
-// 		intakeLeft.moveVelocity(-600);
-// 		intakeRight.moveVelocity(-600);
-//   }
-//   else if (intakeIn.isPressed()){
-//     intakeLeft.moveVelocity(600);
-//     intakeRight.moveVelocity(600);
-//   }
-//   else if (intakeOut.isPressed()){
-//     intakeLeft.moveVelocity(-600);
-//     intakeRight.moveVelocity(-600);
-//   }
-// 	else {
-// 		indexBottom.moveVelocity(0);
-// 		indexTopTwo.moveVelocity(0);
-// 		intakeLeft.moveVelocity(0);
-// 		intakeRight.moveVelocity(0);
-// 	}
-// }
+int overCount = 0;
 
 void index(){
   if(indexIn.isPressed()){
@@ -57,7 +32,34 @@ void intake(){
   }
 }
 
-void turn(int speed, char direction, int deg){
+void turnOver(int speed, char direction, double deg){
+    do{
+      // if(overCount > 0){
+      //   speed = 50;
+      // }
+        double degrees = deg - inertialSensor.get_heading();
+        if(degrees >= 0){
+            frontRight.moveVelocity(-speed);
+            backRight.moveVelocity(-speed);
+            frontLeft.moveVelocity(speed);
+            backLeft.moveVelocity(speed);
+        }
+        else if (degrees < 0){
+            frontRight.moveVelocity(speed);
+            backRight.moveVelocity(speed);
+            frontLeft.moveVelocity(-speed);
+            backLeft.moveVelocity(-speed);
+        }
+        printf("%d\n", degrees);
+        // if(inertialSensor.get_heading() < deg - 5){
+        //   overCount++;
+        // }
+    }while(inertialSensor.get_heading() > (deg + 5) || inertialSensor.get_heading() < (deg - 5));
+
+}
+
+void turn(int speed, char direction, double deg){
+
   if(direction == 'r'){
     while(inertialSensor.get_heading() > (deg + 30) || (inertialSensor.get_heading() < (deg - 30))){
       frontRight.moveVelocity(-speed);
@@ -70,26 +72,43 @@ void turn(int speed, char direction, int deg){
     frontLeft.moveVelocity(0);
     backLeft.moveVelocity(0);
   }
-  else{
-
+  else if(direction == 'l'){
+    while(inertialSensor.get_heading() > (deg + 30) || (inertialSensor.get_heading() < (deg - 30))){
+      frontRight.moveVelocity(speed);
+      backRight.moveVelocity(speed);
+      frontLeft.moveVelocity(-speed);
+      backLeft.moveVelocity(-speed);
+    }
+    frontRight.moveVelocity(0);
+    backRight.moveVelocity(0);
+    frontLeft.moveVelocity(0);
+    backLeft.moveVelocity(0);
   }
 }
 
-void overshoot(char direction, int deg){
-    while(inertialSensor.get_heading() > (deg + 5)){
-      frontRight.moveVelocity(50);
-      backRight.moveVelocity(50);
-      frontLeft.moveVelocity(-50);
-      backLeft.moveVelocity(-50);
+void brake(char braketype){
+  if(braketype = 'b'){
+    frontRight.setBrakeMode(AbstractMotor::brakeMode::brake);
+    frontLeft.setBrakeMode(AbstractMotor::brakeMode::brake);
+    backRight.setBrakeMode(AbstractMotor::brakeMode::brake);
+    backLeft.setBrakeMode(AbstractMotor::brakeMode::brake);
   }
-  frontRight.moveVelocity(0);
-  backRight.moveVelocity(0);
-  frontLeft.moveVelocity(0);
-  backLeft.moveVelocity(0);
+  else if(braketype = 'h'){
+    frontRight.setBrakeMode(AbstractMotor::brakeMode::hold);
+    frontLeft.setBrakeMode(AbstractMotor::brakeMode::hold);
+    backRight.setBrakeMode(AbstractMotor::brakeMode::hold);
+    backLeft.setBrakeMode(AbstractMotor::brakeMode::hold);
+  }
+  else if(braketype = 'c'){
+    frontRight.setBrakeMode(AbstractMotor::brakeMode::coast);
+	  frontLeft.setBrakeMode(AbstractMotor::brakeMode::coast);
+	  backRight.setBrakeMode(AbstractMotor::brakeMode::coast);
+	  backLeft.setBrakeMode(AbstractMotor::brakeMode::coast);
+}
 }
 
-void turnTest(){
-  if(turntester.isPressed()){
-    turn(300, 'r', 90);
+void autontest(){
+  if(autontester.isPressed()){
+    autonomous();
   }
 }
