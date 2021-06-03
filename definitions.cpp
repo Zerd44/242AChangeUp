e48b7cc9451f29d1097f224c3a1eecde805ba125
Zublo54 <56134365+Zublo54@users.noreply.github.com>
2021-06-03 18:03:58 -0400

    Update definitions.cpp

--- definitions.cpp
+++ definitions.cpp
@@ -1,9 +1,14 @@
 #include "main.h"
 
 Motor frontRight(17);
-Motor frontLeft(-18);
+Motor frontLeft(18); //-
 Motor backRight(20);
-Motor backLeft(-19);
+Motor backLeft(19);// -
+
+pros::Motor driveLeftFront(18, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor driveLeftBack(19, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor driveRightFront(17, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor driveRightBack(20, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
 
 Motor indexBottom(14);
 Motor indexTopTwo(13);
@@ -18,5 +23,6 @@ ControllerButton indexOut(ControllerDigital::L2);
 ControllerButton indexIn(ControllerDigital::L1);
 ControllerButton intakeIn(ControllerDigital::R1);
 ControllerButton intakeOut(ControllerDigital::R2);
+ControllerButton calibrater(ControllerDigital::B);
 
 ControllerButton autontester(ControllerDigital::A);
