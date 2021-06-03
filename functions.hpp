b5d967c8a4837ce6b156cd21e3dcf2adb6576ac9
Zublo54 <56134365+Zublo54@users.noreply.github.com>
2021-06-03 18:02:57 -0400

    Update functions.hpp

--- functions.hpp
+++ functions.hpp
@@ -13,3 +13,9 @@ extern void turnOver(int speed, char direction, double deg);
 extern void brake(char braketype);
 
 extern void autontest();
+
+extern void setDrive(int left, int right);
+
+extern void rotate(double angle, int number);
+
+extern void calibrate();
